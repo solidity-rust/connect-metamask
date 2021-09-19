ec2910e572f78590df0f0fccbe39421fda1c0b90
solidity-rust <soliditymaster@outlook.com>
2021-09-19 12:50:48 -0700

    complete

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
-const HelloWorld = artifacts.require("HelloWorld");
-
-
-module.exports = function(deployer) {
-  deployer.deploy(HelloWorld);
-  deployer.deploy(SimpleStorage);
-};
